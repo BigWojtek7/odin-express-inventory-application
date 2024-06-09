e3c3c73092269d75bad574db5c691dcd4f5063df
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-06-09 15:51:31 +0200

    Add routes to catalog.js

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,4 +1,60 @@
 const express = require('express');
 const router = express.Router();
 
-const
+const item_controller = require('../controllers/itemController');
+const category_controller = require('../controllers/categoryController');
+
+/// ITEM ROUTES ///
+
+// GET catalog Home Page
+router.get('/', item_controller.index);
+
+// Create item on get
+router.get('/item/create', item_controller.item_create_get);
+
+// POST request for creating item.
+router.post('/item/create', item_controller.item_create_post);
+
+// GET request to delete item.
+router.get('/item/:id/delete', item_controller.item_delete_get);
+
+// POST request to delete item.
+router.post('/item/:id/delete', item_controller.item_delete_post);
+
+// GET request to update item.
+router.get('/item/:id/update', item_controller.item_update_get);
+
+// POST request to update item.
+router.post('/item/:id/update', item_controller.item_update_post);
+
+// GET request for one item.
+router.get('/item/:id', item_controller.item_detail);
+
+// GET request for list of all item items.
+router.get('/items', item_controller.item_list);
+
+/// CATALOG ROUTES ///
+
+// Get request for creating catalog
+router.get('/catalog/create', category_controller.category_create_get);
+
+// Post for create catalog
+router.post('/catalog/create', category_controller.category_create_post);
+
+// GET request to delete Genre.
+router.get('/genre/:id/delete', genre_controller.genre_delete_get);
+
+// POST request to delete Genre.
+router.post('/genre/:id/delete', genre_controller.genre_delete_post);
+
+// GET request to update Genre.
+router.get('/genre/:id/update', genre_controller.genre_update_get);
+
+// POST request to update Genre.
+router.post('/genre/:id/update', genre_controller.genre_update_post);
+
+// GET request for one Genre.
+router.get('/genre/:id', genre_controller.genre_detail);
+
+// GET request for list of all Genre.
+router.get('/genres', genre_controller.genre_list);
